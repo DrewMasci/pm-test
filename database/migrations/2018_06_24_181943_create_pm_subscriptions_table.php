4b8f8de5422933a9fe56ef7f863a4d68c8b80908
BuildTools <unconfigured@null.spigotmc.org>
2018-06-26 23:26:30 +0100

    renamed the table to be more sensible

--- database/migrations/2018_06_24_181943_create_pm_subscriptions_table.php
+++ database/migrations/2018_06_24_181943_create_pm_subscriptions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePmTestApiTable extends Migration
+class CreatePmSubscriptions extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePmTestApiTable extends Migration
      */
     public function up()
     {
-        Schema::create('pm_test_api_table', function (Blueprint $table) {
+        Schema::create('pm_subscriptions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('msisdn');
             $table->string('product_id');
@@ -29,6 +29,6 @@ class CreatePmTestApiTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pm_test_api_table');
+        Schema::dropIfExists('pm_subscriptions');
     }
 }

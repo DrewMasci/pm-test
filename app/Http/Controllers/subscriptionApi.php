e36457c4c4a342dba2c23659f60d9277d9138280
BuildTools <unconfigured@null.spigotmc.org>
2018-06-28 19:24:47 +0100

    rebuilt the controller, put the private functions to the top and public ones to the bottom. Made saving the data it's own separate function which validates the data doesn't already exist. Altered the validation of the URI prams to make the  array optional and improved on the error handling.

--- app/Http/Controllers/subscriptionApi.php
+++ app/Http/Controllers/subscriptionApi.php
@@ -3,14 +3,89 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\pmSubscriptions;
 
 class subscriptionApi extends Controller
 {
+    private function validateUriParams($manditory = [], $optional = [])
+    {
+        $error_messages = [];
+
+        if(sizeof($manditory) === 0 && sizeof($optional) === 0) {
+            $error_messages[] = "no attribute data passed through in request";
+            return $error_messages;
+        }
+
+        foreach($manditory as $index => $value) {
+            if($value === null) {
+              $error_messages[] = "manditory attribute, $index, not set";
+            }
+        }
+        if(sizeof($error_messages) !== 0) {
+            return $error_messages;
+        }
+
+        foreach($optional as $index => $value) {
+            if($value === null) {
+              $error_messages[] = "optional attribute, $index, not set";
+            }
+        }
+        if(sizeof($error_messages) === sizeof($optional)) {
+            return $error_messages;
+        }
+
+        return true;
+    }
+
+    private function saveData($data)
+    {
+        $subscription = new pmSubscriptions();
+
+        foreach($data as $field => $value) {
+            $subscription = $subscription->where($field, $value);
+        }
+
+        $doesExist = $subscription->get();
+
+        if(sizeof($doesExist) > 0) {
+            return [
+                'response' => 'failed',
+                'error' => 'a record with these exact details already exists'
+           ];
+        }
+
+        $subscription = new pmSubscriptions();
+
+        foreach($data as $field => $value) {
+            $subscription->{$field} = $value;
+        }
+        $subscription->created_at = time();
+        $subscription->save();
+
+        return [
+            'response' => 'subscription added'
+       ];
+    }
+
     public function addSubscription(Request $request)
     {
-        $return = [
-            'response' => 'working'
+        $data = [
+            'msisdn' => $request->input('msisdn'),
+            'product_id' => $request->input('product_id')
         ];
+
+        $response = $this->validateUriParams($data);
+
+        if($response !== true) {
+            $return = [
+              'response' => 'failed',
+              'error_messages' => $response
+            ];
+
+            return response()->json($return);
+        }
+
+        $return = $this->saveData($data);
 
         return response()->json($return);
     }

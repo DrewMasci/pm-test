dfacd17b972b91976a15597afa6205c30a306cda
BuildTools <unconfigured@null.spigotmc.org>
2018-06-28 18:12:35 +0100

    changed the location of the model for the subscriptions as well as named it something better.

--- app/Models/pmSubscriptions.php
+++ app/Models/pmSubscriptions.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class pm_test_api_table extends Model
+class pmSubscriptions extends Model
 {
     //
 }

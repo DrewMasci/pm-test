1945a4d1f89ab0997c738e77339892614037fc09
BuildTools <unconfigured@null.spigotmc.org>
2018-06-28 18:05:01 +0100

    fixed issue with the class not following the same naming convention as the migration itself.

--- database/migrations/2018_06_24_181943_create_pm_subscriptions_table.php
+++ database/migrations/2018_06_24_181943_create_pm_subscriptions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePmSubscriptions extends Migration
+class CreatePmSubscriptionsTable extends Migration
 {
     /**
      * Run the migrations.

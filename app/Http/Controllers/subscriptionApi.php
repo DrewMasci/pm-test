d854ed4b3e12e3e2ca8511e3558dd701ffb475e5
BuildTools <unconfigured@null.spigotmc.org>
2018-07-01 19:26:15 +0100

    restructured both files.

--- app/Http/Controllers/subscriptionApi.php
+++ app/Http/Controllers/subscriptionApi.php
@@ -7,98 +7,56 @@ use App\Models\pmSubscriptions;
 
 class subscriptionApi extends Controller
 {
-    /*-- Private Functions --*/
-
-    private function validateUriParams($manditory = [], $optional = [])
+    public function addSubscription(Request $request)
     {
-        $error_messages = [];
-
-        if(sizeof($manditory) === 0 && sizeof($optional) === 0) {
-            $error_messages[] = "no attribute data passed through in request";
-            return $error_messages;
-        }
-
-        foreach($manditory as $index => $value) {
-            if($value === null) {
-              $error_messages[] = "manditory attribute, $index, not set";
-            }
-        }
-        if(sizeof($error_messages) !== 0) {
-            return $error_messages;
-        }
-
-        foreach($optional as $index => $value) {
-            if($value === null) {
-              $error_messages[] = "optional attribute, $index, not set";
-            }
-        }
-        if(sizeof($error_messages) === sizeof($optional) && !empty($optional)) {
-            return $error_messages;
-        }
-
-        return true;
-    }
-
-    private function expandMsisdn($msisdn)
-    {
-        if(is_array($msisdn) || substr($msisdn, 0, 1) === 'A') return $msisdn;
-
-        $alias = file_get_contents('http://interview.pmcservices.co.uk/alias/lookup?msisdn=' . $msisdn);
-
-        $temp = [$msisdn];
-
-        if(is_string($alias) && strlen($alias) === 13) {
-            $temp[] = $alias;
-        }
-
-        if(strpos($msisdn, '+44') === 0) {
-            $converted_msisdn = str_replace('+44', '0', $msisdn);
-
-            $temp[] = $converted_msisdn;
-            $alias = file_get_contents('http://interview.pmcservices.co.uk/alias/lookup?msisdn=' . $converted_msisdn);
-        }
-        else if(substr($msisdn, 0, 1) === '0') {
-            $converted_msisdn = '+44' . substr($msisdn, 1);
-
-            $temp[] = $converted_msisdn;
-            $alias = file_get_contents('http://interview.pmcservices.co.uk/alias/lookup?msisdn=' . $converted_msisdn);
-        }
-
-        if(is_string($alias) && strlen($alias) === 13 && !in_array($alias, $temp)) {
-            $temp[] = $alias;
-        }
-
-        return $temp;
-    }
-
-    private function saveData($data)
-    {
-        $search_data = $data;
-
-        if(isset($search_data['msisdn'])) {
-            $search_data['msisdn'] = $this->expandMsisdn($search_data['msisdn']);
-        }
-
-        $doesExist = $this->searchForData($search_data, 'AND', true);
-
-        if(sizeof($doesExist) > 0) {
-            return [
-                'response' => 'failed',
-                'error' => 'a record with these exact details already exists'
-           ];
-        }
-
-        $subscription = new pmSubscriptions();
-
-        foreach($data as $field => $value) {
-            $subscription->{$field} = $value;
-        }
-        $subscription->created_at = time();
-        $subscription->save();
-
-        return [
-            'response' => 'subscription added'
+        $data = [
+            'msisdn' => $request->input('msisdn'),
+            'product_id' => $request->input('product_id')
         ];
+
+        $sub = new pmSubscriptions();
+        $response = $this->needToErrorOut($sub->validateUriParams($data));
+
+        if($response !== null) return $response;
+
+        return response()->json($sub->saveData($data));
+    }
+
+    public function search(Request $request)
+    {
+        $data = [
+            'msisdn' => $request->input('msisdn'),
+            'product_id' => $request->input('product_id')
+        ];
+
+        $sub = new pmSubscriptions();
+        $response = $this->needToErrorOut($sub->validateUriParams([], $data));
+
+        if($response !== null) return $response;
+
+        return response()->json($sub->searchForData($data));
+    }
+
+    public function deleteSubscription(Request $request)
+    {
+        $manditory_data = [
+            'msisdn' => $request->input('msisdn'),
+        ];
+
+        $optional_data = [
+            'product_id' => $request->input('product_id')
+        ];
+
+        $sub = new pmSubscriptions();
+        $response = $this->needToErrorOut($sub->validateUriParams($manditory_data, $optional_data));
+
+        $data = $this->arrayMergeNotNull($manditory_data, $optional_data);
+
+        dd($data);
+
+        if($response !== null) return $response;
+
+        return response()->json($sub->deleteData($data));
     }
 
     private function needToErrorOut($response)
@@ -115,91 +73,6 @@ class subscriptionApi extends Controller
         return null;
     }
 
-    private function deleteData($data)
-    {
-        if(isset($data['msisdn'])) {
-            $data['msisdn'] = $this->expandMsisdn($data['msisdn']);
-        }
-
-        $records = $this->searchForData($data, 'AND', true);
-
-        if(sizeof($records) === 0) {
-            return [
-                'response' => 'failed',
-                'error' => 'no record with these details exists'
-           ];
-        }
-
-        if(sizeof($records) > 1 && isset($data['product_id'])) {
-            return [
-                'response' => 'failed',
-                'error' => 'more than one record with these details exists'
-           ];
-        }
-
-        $subscription = new pmSubscriptions();
-        $subscription->where('id', $records[0]->id)
-            ->update(['deleted_at' => date('Y-m-d H:i:s')]);
-
-        return [
-            'response' => 'subscription deleted'
-        ];
-    }
-
-    private function searchForData($data, $operator = 'OR', $bypass_error = false)
-    {
-        $subscription = new pmSubscriptions();
-
-        if(isset($data['msisdn'])) {
-            $data['msisdn'] = $this->expandMsisdn($data['msisdn']);
-        }
-
-        foreach($data as $field => $value) {
-            if($value !== null) {
-                if(stripos($subscription->toSql(), 'where') === false || $operator === 'AND') {
-                    $subscription = $subscription->where($field, $value);
-                }
-                else {
-                    $subscription = $subscription->orWhere($field, $value);
-                }
-            }
-        }
-        if($bypass_error) $subscription = $subscription->whereNull('deleted_at');
-
-        $results = $subscription->get();
-
-        if(sizeof($results) === 0 && !$bypass_error) {
-            return [
-                'respose' => 'failed',
-                'error' => 'No results found'
-            ];
-        }
-        return $results;
-    }
-
-    private function coolSearchForData($data)
-    {
-        $subscription = new pmSubscriptions();
-
-        if(isset($data['msisdn'])) {
-            $data['msisdn'] = $this->expandMsisdn($data['msisdn']);
-        }
-
-        foreach($data as $field => $value) {
-            if($value !== null) {
-                if(stripos($subscription->toSql(), 'where') === false) {
-                    $subscription = $subscription->where($field, 'like', '%'. $value .'%');
-                }
-                else {
-                    $subscription = $subscription->orWhere($field, 'like', '%'. $value .'%');
-                }
-            }
-        }
-        $subscription = $subscription->whereNull('deleted_at');
-
-        $doesExist = $subscription->get();
-    }
-
     private function arrayMergeNotNull()
     {
         $temp = [];
@@ -213,56 +86,5 @@ class subscriptionApi extends Controller
         }
 
         return $temp;
-    }
-
-    /*-- Public Functions --*/
-
-    public function addSubscription(Request $request)
-    {
-        $data = [
-            'msisdn' => $request->input('msisdn'),
-            'product_id' => $request->input('product_id')
-        ];
-
-        $response = $this->needToErrorOut($this->validateUriParams($data));
-
-        if($response !== null) return $response;
-
-        return response()->json($this->saveData($data));
-    }
-
-    public function search(Request $request)
-    {
-        $data = [
-            'msisdn' => $request->input('msisdn'),
-            'product_id' => $request->input('product_id')
-        ];
-
-        $response = $this->needToErrorOut($this->validateUriParams([], $data));
-
-        if($response !== null) return $response;
-
-        return response()->json($this->searchForData($data));
-    }
-
-    public function deleteSubscription(Request $request)
-    {
-        $manditory_data = [
-            'msisdn' => $request->input('msisdn'),
-        ];
-
-        $optional_data = [
-            'product_id' => $request->input('product_id')
-        ];
-
-        $response = $this->needToErrorOut($this->validateUriParams($manditory_data, $optional_data));
-
-        $data = $this->arrayMergeNotNull($manditory_data, $optional_data);
-
-        dd($data);
-
-        if($response !== null) return $response;
-
-        return response()->json($this->deleteData($data));
     }
 }
--- app/Models/pmSubscriptions.php
+++ app/Models/pmSubscriptions.php
@@ -6,5 +6,180 @@ use Illuminate\Database\Eloquent\Model;
 
 class pmSubscriptions extends Model
 {
-    //
+    public function validateUriParams($manditory = [], $optional = [])
+    {
+        $error_messages = [];
+
+        if(sizeof($manditory) === 0 && sizeof($optional) === 0) {
+            $error_messages[] = "no attribute data passed through in request";
+            return $error_messages;
+        }
+
+        foreach($manditory as $index => $value) {
+            if($value === null) {
+              $error_messages[] = "manditory attribute, $index, not set";
+            }
+        }
+        if(sizeof($error_messages) !== 0) {
+            return $error_messages;
+        }
+
+        foreach($optional as $index => $value) {
+            if($value === null) {
+              $error_messages[] = "optional attribute, $index, not set";
+            }
+        }
+        if(sizeof($error_messages) === sizeof($optional) && !empty($optional)) {
+            return $error_messages;
+        }
+
+        return true;
+    }
+
+    private function expandMsisdn($msisdn)
+    {
+        if(is_array($msisdn) || substr($msisdn, 0, 1) === 'A') return $msisdn;
+
+        $alias = file_get_contents('http://interview.pmcservices.co.uk/alias/lookup?msisdn=' . $msisdn);
+
+        $temp = [$msisdn];
+
+        if(is_string($alias) && strlen($alias) === 13) {
+            $temp[] = $alias;
+        }
+
+        if(strpos($msisdn, '+44') === 0) {
+            $converted_msisdn = str_replace('+44', '0', $msisdn);
+
+            $temp[] = $converted_msisdn;
+            $alias = file_get_contents('http://interview.pmcservices.co.uk/alias/lookup?msisdn=' . $converted_msisdn);
+        }
+        else if(substr($msisdn, 0, 1) === '0') {
+            $converted_msisdn = '+44' . substr($msisdn, 1);
+
+            $temp[] = $converted_msisdn;
+            $alias = file_get_contents('http://interview.pmcservices.co.uk/alias/lookup?msisdn=' . $converted_msisdn);
+        }
+
+        if(is_string($alias) && strlen($alias) === 13 && !in_array($alias, $temp)) {
+            $temp[] = $alias;
+        }
+
+        return $temp;
+    }
+
+    public function saveData($data)
+    {
+        $search_data = $data;
+
+        if(isset($search_data['msisdn'])) {
+            $search_data['msisdn'] = $this->expandMsisdn($search_data['msisdn']);
+        }
+
+        $doesExist = $this->searchForData($search_data, 'AND', true);
+
+        if(sizeof($doesExist) > 0) {
+            return [
+                'response' => 'failed',
+                'error' => 'a record with these exact details already exists'
+           ];
+        }
+
+        $subscription = new pmSubscriptions();
+
+        foreach($data as $field => $value) {
+            $subscription->{$field} = $value;
+        }
+        $subscription->created_at = time();
+        $subscription->save();
+
+        return [
+            'response' => 'subscription added'
+        ];
+    }
+
+    public function deleteData($data)
+    {
+        if(isset($data['msisdn'])) {
+            $data['msisdn'] = $this->expandMsisdn($data['msisdn']);
+        }
+
+        $records = $this->searchForData($data, 'AND', true);
+
+        if(sizeof($records) === 0) {
+            return [
+                'response' => 'failed',
+                'error' => 'no record with these details exists'
+           ];
+        }
+
+        if(sizeof($records) > 1 && isset($data['product_id'])) {
+            return [
+                'response' => 'failed',
+                'error' => 'more than one record with these details exists'
+           ];
+        }
+
+        $subscription = new pmSubscriptions();
+        $subscription->where('id', $records[0]->id)
+            ->update(['deleted_at' => date('Y-m-d H:i:s')]);
+
+        return [
+            'response' => 'subscription deleted'
+        ];
+    }
+
+    public function searchForData($data, $operator = 'OR', $bypass_error = false)
+    {
+        $subscription = new pmSubscriptions();
+
+        if(isset($data['msisdn'])) {
+            $data['msisdn'] = $this->expandMsisdn($data['msisdn']);
+        }
+
+        foreach($data as $field => $value) {
+            if($value !== null) {
+                if(stripos($subscription->toSql(), 'where') === false || $operator === 'AND') {
+                    $subscription = $subscription->where($field, $value);
+                }
+                else {
+                    $subscription = $subscription->orWhere($field, $value);
+                }
+            }
+        }
+        if($bypass_error) $subscription = $subscription->whereNull('deleted_at');
+
+        $results = $subscription->get();
+
+        if(sizeof($results) === 0 && !$bypass_error) {
+            return [
+                'respose' => 'failed',
+                'error' => 'No results found'
+            ];
+        }
+        return $results;
+    }
+
+    public function coolSearchForData($data)
+    {
+        $subscription = new pmSubscriptions();
+
+        if(isset($data['msisdn'])) {
+            $data['msisdn'] = $this->expandMsisdn($data['msisdn']);
+        }
+
+        foreach($data as $field => $value) {
+            if($value !== null) {
+                if(stripos($subscription->toSql(), 'where') === false) {
+                    $subscription = $subscription->where($field, 'like', '%'. $value .'%');
+                }
+                else {
+                    $subscription = $subscription->orWhere($field, 'like', '%'. $value .'%');
+                }
+            }
+        }
+        $subscription = $subscription->whereNull('deleted_at');
+
+        $doesExist = $subscription->get();
+    }
 }
